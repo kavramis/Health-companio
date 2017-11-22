7207ce78f6ed1ddc9d4afa537715c899b6208ba9
Sam <basilis.samoladas@gmail.com>
2017-11-22 23:34:01 +0200

    ImgChange

--- HealthCompanion_version1.0/HealthCompanion_version1.0/StartUI.Designer.cs
+++ HealthCompanion_version1.0/HealthCompanion_version1.0/StartUI.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.LoginPanel = new System.Windows.Forms.Panel();
+            this.RegisterPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.RegisterBtn = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.RegisterBtn = new System.Windows.Forms.Button();
-            this.LoginBtn = new System.Windows.Forms.Button();
-            this.RegisterPanel = new System.Windows.Forms.Panel();
-            this.LoginPanel = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.RegisterPanel.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
-            this.RegisterPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -50,10 +50,73 @@
             this.panel1.Controls.Add(this.RegisterPanel);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(491, 0);
+            this.panel1.Location = new System.Drawing.Point(655, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(490, 657);
+            this.panel1.Size = new System.Drawing.Size(653, 809);
             this.panel1.TabIndex = 0;
+            // 
+            // LoginPanel
+            // 
+            this.LoginPanel.Location = new System.Drawing.Point(127, 565);
+            this.LoginPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LoginPanel.Name = "LoginPanel";
+            this.LoginPanel.Size = new System.Drawing.Size(408, 123);
+            this.LoginPanel.TabIndex = 2;
+            // 
+            // RegisterPanel
+            // 
+            this.RegisterPanel.Controls.Add(this.label1);
+            this.RegisterPanel.Location = new System.Drawing.Point(127, 169);
+            this.RegisterPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RegisterPanel.Name = "RegisterPanel";
+            this.RegisterPanel.Size = new System.Drawing.Size(408, 351);
+            this.RegisterPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.LoginBtn);
+            this.panel3.Controls.Add(this.RegisterBtn);
+            this.panel3.Location = new System.Drawing.Point(127, 76);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(408, 54);
+            this.panel3.TabIndex = 0;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.FlatAppearance.BorderSize = 0;
+            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginBtn.Location = new System.Drawing.Point(204, 6);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(196, 44);
+            this.LoginBtn.TabIndex = 1;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            // 
+            // RegisterBtn
+            // 
+            this.RegisterBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.RegisterBtn.FlatAppearance.BorderSize = 0;
+            this.RegisterBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RegisterBtn.Location = new System.Drawing.Point(4, 6);
+            this.RegisterBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RegisterBtn.Name = "RegisterBtn";
+            this.RegisterBtn.Size = new System.Drawing.Size(196, 44);
+            this.RegisterBtn.TabIndex = 0;
+            this.RegisterBtn.Text = "Register";
+            this.RegisterBtn.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -61,91 +124,40 @@
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(490, 657);
+            this.panel2.Size = new System.Drawing.Size(653, 809);
             this.panel2.TabIndex = 1;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::HealthCompanion_version1._0.Properties.Resources.fat_mus1;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(490, 654);
+            this.pictureBox1.Size = new System.Drawing.Size(653, 805);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.LoginBtn);
-            this.panel3.Controls.Add(this.RegisterBtn);
-            this.panel3.Location = new System.Drawing.Point(95, 62);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(306, 44);
-            this.panel3.TabIndex = 0;
-            // 
-            // RegisterBtn
-            // 
-            this.RegisterBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.RegisterBtn.FlatAppearance.BorderSize = 0;
-            this.RegisterBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.RegisterBtn.Location = new System.Drawing.Point(3, 5);
-            this.RegisterBtn.Name = "RegisterBtn";
-            this.RegisterBtn.Size = new System.Drawing.Size(147, 36);
-            this.RegisterBtn.TabIndex = 0;
-            this.RegisterBtn.Text = "Register";
-            this.RegisterBtn.UseVisualStyleBackColor = true;
-            // 
-            // LoginBtn
-            // 
-            this.LoginBtn.FlatAppearance.BorderSize = 0;
-            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.LoginBtn.Location = new System.Drawing.Point(153, 5);
-            this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(147, 36);
-            this.LoginBtn.TabIndex = 1;
-            this.LoginBtn.Text = "Login";
-            this.LoginBtn.UseVisualStyleBackColor = true;
-            // 
-            // RegisterPanel
-            // 
-            this.RegisterPanel.Controls.Add(this.label1);
-            this.RegisterPanel.Location = new System.Drawing.Point(95, 137);
-            this.RegisterPanel.Name = "RegisterPanel";
-            this.RegisterPanel.Size = new System.Drawing.Size(306, 285);
-            this.RegisterPanel.TabIndex = 1;
-            // 
-            // LoginPanel
-            // 
-            this.LoginPanel.Location = new System.Drawing.Point(95, 459);
-            this.LoginPanel.Name = "LoginPanel";
-            this.LoginPanel.Size = new System.Drawing.Size(306, 100);
-            this.LoginPanel.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 11);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
             // StartUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(981, 657);
+            this.ClientSize = new System.Drawing.Size(1308, 809);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "StartUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "StartUI";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel3.ResumeLayout(false);
             this.RegisterPanel.ResumeLayout(false);
             this.RegisterPanel.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
