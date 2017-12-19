98e0a40d38e583a452fd91713f9277d29b7d5a5a
leo tsap <ltsapardakis@gmail.com>
2017-12-19 23:33:38 +0200

    Create UI for Trainning Schedule

--- HealthCompanion_version1.0/HealthCompanion_version1.0/Program.cs
+++ HealthCompanion_version1.0/HealthCompanion_version1.0/Program.cs
@@ -16,7 +16,7 @@ namespace HealthCompanion_version1._0
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StartUI());
+            Application.Run(new TrainningForm());
         }
     }
 }
