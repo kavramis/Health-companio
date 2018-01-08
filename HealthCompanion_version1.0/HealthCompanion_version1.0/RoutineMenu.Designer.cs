83070fbac2cb3641e6057b44a49e7edfd62ed93e
Sam <basilis.samoladas@gmail.com>
2018-01-08 02:10:21 +0200

    UI Beta 10

--- HealthCompanion_version1.0/HealthCompanion_version1.0/RoutineMenu.Designer.cs
+++ HealthCompanion_version1.0/HealthCompanion_version1.0/RoutineMenu.Designer.cs
@@ -312,11 +312,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(12, 110);
+            this.label4.Location = new System.Drawing.Point(-5, 104);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(251, 32);
+            this.label4.Size = new System.Drawing.Size(217, 34);
             this.label4.TabIndex = 1;
             this.label4.Text = "Health Companion";
             // 
@@ -413,7 +413,7 @@
             this.button3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.Maroon;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(-3, 216);
+            this.button3.Location = new System.Drawing.Point(0, 229);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(200, 46);
             this.button3.TabIndex = 3;
