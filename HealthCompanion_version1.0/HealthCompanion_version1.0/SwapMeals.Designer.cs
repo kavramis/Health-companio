2e92cbc05e9d22cd82aefc69c51639fbd1044aff
Κωνσταντινος Αβραμης <K.Avramis@hotmail.com>
2018-01-08 17:58:39 +0200

    Μας τα κανε το git

--- HealthCompanion_version1.0/HealthCompanion_version1.0/SwapMeals.Designer.cs
+++ HealthCompanion_version1.0/HealthCompanion_version1.0/SwapMeals.Designer.cs
@@ -74,11 +74,12 @@
             this.mealNumberDataGridViewTextBoxColumn,
             this.mealDayDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.dietPlanFoodBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(413, 14);
+            this.dataGridView1.Location = new System.Drawing.Point(310, 11);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(683, 160);
+            this.dataGridView1.Size = new System.Drawing.Size(512, 130);
             this.dataGridView1.TabIndex = 0;
             // 
             // dietPlanIDDataGridViewTextBoxColumn
@@ -125,11 +126,12 @@
             this.caloriesDataGridViewTextBoxColumn1,
             this.typeDataGridViewTextBoxColumn1});
             this.add.DataSource = this.foodsBindingSource;
-            this.add.Location = new System.Drawing.Point(413, 346);
+            this.add.Location = new System.Drawing.Point(310, 281);
+            this.add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.add.MultiSelect = false;
             this.add.Name = "add";
             this.add.RowTemplate.Height = 24;
-            this.add.Size = new System.Drawing.Size(683, 183);
+            this.add.Size = new System.Drawing.Size(512, 149);
             this.add.TabIndex = 1;
             // 
             // foodIDDataGridViewTextBoxColumn2
@@ -171,10 +173,11 @@
             this.caloriesDataGridViewTextBoxColumn,
             this.typeDataGridViewTextBoxColumn});
             this.old.DataSource = this.foodsDietPlanFoodBindingSource;
-            this.old.Location = new System.Drawing.Point(413, 180);
+            this.old.Location = new System.Drawing.Point(310, 146);
+            this.old.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.old.MultiSelect = false;
             this.old.Name = "old";
-            this.old.Size = new System.Drawing.Size(683, 160);
+            this.old.Size = new System.Drawing.Size(512, 130);
             this.old.TabIndex = 2;
             // 
             // foodIDDataGridViewTextBoxColumn1
@@ -208,9 +211,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(274, 346);
+            this.button1.Location = new System.Drawing.Point(170, 271);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 50);
+            this.button1.Size = new System.Drawing.Size(100, 41);
             this.button1.TabIndex = 3;
             this.button1.Text = "Swap my foods";
             this.button1.UseVisualStyleBackColor = true;
@@ -218,9 +222,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(274, 450);
+            this.button2.Location = new System.Drawing.Point(206, 366);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 79);
+            this.button2.Size = new System.Drawing.Size(100, 64);
             this.button2.TabIndex = 4;
             this.button2.Text = "Save my program";
             this.button2.UseVisualStyleBackColor = true;
@@ -248,15 +253,16 @@
             // 
             // SwapMeals
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1108, 545);
+            this.ClientSize = new System.Drawing.Size(831, 443);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.old);
             this.Controls.Add(this.add);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SwapMeals";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SwapMeals";
